d8f53de45784b112c4f5b04c2c0059e74ce938f4
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2021-09-27 18:01:39 +0700

    commit new version

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+'''
+Space AC Ground Control Station
+~~~~~~~~~~~~~~~~~~~
+A basic ground control station need for the Space AC.
+:copyright: (c) 2021-present Retaehc_pop
+:license: MIT, see LICENSE for more details.
+'''
+
 from ._global import *
 
+__title__ = 'Spaceac tools'
+__author__ = 'Retaehc_pop'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-present Retaehc_pop'
 __version__ = "0.0.1"
