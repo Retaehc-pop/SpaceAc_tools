e8f8e5ee2f3f7c22c94a5b7f4f79874e9aea0671
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2021-09-27 13:51:32 +0700

    commit

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
 from setuptools import setup
+import re
+
+
+requirements = []
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = ''
+with open('__init__.py') as f:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+readme = ''
+with open('readme.md') as f:
+    readme = f.read()
+
 setup(name='SpaceAc_tools',
-      version='0.1',
-      description='A cutom packet for Space AC Ground Control Station',
-      url='#',
+      version=version,
+      description='A Python wrapper for the Space c Ground station',
+      long_description=readme,
+      url='https://github.com/Retaehc-pop/SpaceAc_tools',
       author='Retaehc_pop',
       author_email='Papop2003@gmail.com',
       license='MIT',
       packages=['SpaceAC_tools'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=requirements,
+      python_requires='>3.8.0')
